6dc7f3088a622736d6f7ad32aa60c108bed076e7
Simachew <simachewdenekewse@gmail.com>
2024-03-11 22:25:55 +0300

    Building portifolio - admin add ons messages, aboutme, navabar

--- frontend/src/admin/components/NavBar.js
+++ frontend/src/admin/components/NavBar.js
@@ -1,27 +1,49 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-            <nav className="bg-gray-800 p-4 mb-12 text-white">
-                
-                <ul className="flex justify-center space-x-4">
-                    <li>
-                        <Link to="/admin">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/projects">Projects</Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/skills">Skills</Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/about">About Me</Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/messages">Messages</Link>
-                    </li>
-                </ul>
-            </nav>
+        <nav className="bg-gray-800 p-4 mb-12 text-white">
+            <div className="container mx-auto">
+                <div className="flex justify-between">
+                    <Link to="/admin" className="text-white font-bold text-lg">Admin</Link>
+                    {/* Hamburger menu icon for mobile */}
+                    <button onClick={toggleMenu} className="lg:hidden">
+                        <svg className="w-6 h-6 fill-current text-white" viewBox="0 0 24 24">
+                            {isMenuOpen ? (
+                                <path d="M6 18L18 6M6 6l12 12" />
+                            ) : (
+                                <path d="M4 6h16M4 12h16m-7 6h7" />
+                            )}
+                        </svg>
+                    </button>
+                    {/* Navigation links */}
+                    <ul className={`lg:flex lg:items-center lg:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
+                        <li>
+                            <Link to="/admin" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/projects" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4">Projects</Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/skills" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4">Skills</Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/about" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4">About Me</Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/messages" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400">Messages</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
--- frontend/src/admin/pages/AboutMe.js
+++ frontend/src/admin/pages/AboutMe.js
@@ -1,9 +1,104 @@
+import { useState, useEffect } from 'react';
+
 const AboutMe = () => {
-    return ( 
-        <div className="mt-8">
-            About
-        </div>
-     );
-}
- 
+  const [aboutMe, setAboutMe] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [editForms, setEditForms] = useState({});
+
+  useEffect(() => {
+    fetchSections();
+  }, []);
+
+  const fetchSections = async () => {
+    try {
+      const response = await fetch("http://localhost:10000/sime/api");
+      if (!response.ok) {
+        throw new Error('Failed to fetch about sections');
+      }
+      const data = await response.json();
+      const abouts = await data.about;
+      const about = abouts[0];
+      setAboutMe(about);
+      // Initialize editForms state
+      const initialEditForms = {};
+      Object.keys(about).forEach(key => {
+        initialEditForms[key] = false;
+      });
+      setEditForms(initialEditForms);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const toggleEditForm = (key) => {
+    setEditForms(prevState => ({
+      ...prevState,
+      [key]: !prevState[key]
+    }));
+  };
+
+  const handleFormSubmit = async (key) => {
+    try {
+      const response = await fetch(`http://localhost:10000/sime/api/about/${key}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ [key]: aboutMe[key] }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to save data');
+      }
+      toggleEditForm(key);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  if (loading) return <p className='text-center text-3xl'>Loading...</p>;
+  if (error) return <p className='text-center text-3xl'>Error: {error}</p>;
+
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-24 pt-8">
+      {Object.keys(aboutMe).map(key => (
+        key !== '_id' && key !== '__v' && (
+          <div key={key} className="bg-white rounded-lg overflow-hidden shadow-md mb-4 md:mx-auto md:w-2/3">
+            <div className="px-6 py-4">
+              <h2 className="text-3xl text-center mb-12 text-blue-700 font-bold">{key}</h2>
+              {editForms[key] ? (
+                <form className="text-center" onSubmit={(e) => { e.preventDefault(); handleFormSubmit(key); }}>
+                  <textarea 
+                    className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" 
+                    rows="5"
+                    value={aboutMe[key]}
+                    onChange={(e) => setAboutMe({...aboutMe, [key]: e.target.value})}
+                  />
+                  <div className="mt-4 flex justify-center">
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                      Save
+                    </button>
+                    <button type="button" onClick={() => toggleEditForm(key)} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded ml-2">
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              ) : (
+                <div className='flex flex-col lg:flex-row justify-center lg:justify-between'>
+                  <p className="text-center lg:text-left text-gray-700 text-base mb-4 lg:mb-0">{aboutMe[key]}</p>
+                  <button onClick={() => toggleEditForm(key)} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4 lg:mt-0">
+                    Edit
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        )
+      ))}
+    </div>
+  );
+};
+
 export default AboutMe;
